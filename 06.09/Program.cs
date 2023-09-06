e3d4260747746832392aa1ffa2e3c76cb6648dae
stepan365 <143639328+stepan365@users.noreply.github.com>
2023-09-06 17:41:15 +0300

    Добавьте файлы проекта.

---
+++ 06.09/Program.cs
@@ -0,0 +1,31 @@
+﻿int odd = 0; // ne parni
+int even = 0; // parni
+
+int Nep = 0;
+int Par = 0;
+Console.WriteLine(" перше число діапазону : ") ;
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("другке число діапазону: ");
+int limit = int.Parse(Console.ReadLine());
+
+while (a <= limit)
+{
+    if (a % 2 == 0)
+    {
+        even++;
+        Par += a;
+
+    }
+    else
+    {
+        odd++;
+        Nep += a;
+    }
+    a++;
+}
+
+Console.WriteLine(" кільківсь не парних: " + odd );
+Console.WriteLine(" кількість парних:  " + even);
+Console.WriteLine(" сума не парних " + Nep);
+Console.WriteLine(" сума парних " + Par);
+Console.ReadLine();
